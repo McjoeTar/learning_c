159e8d0c48aa9e1f0552ebf35f3fa988c4553dd6
McjoeTar <85073169+McjoeTar@users.noreply.github.com>
2024-06-12 23:33:54 +0300

    did question 1

---
+++ HW1/ex1_q1_313432403.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 0,i;
+	printf("Input: n = ");
+    scanf("%d", &n);
+    printf("Output: ");
+    for(i=0;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            printf("%d ",&i);
+        }
+    }
+    printf("%d",n);
+	return 0;
+}
